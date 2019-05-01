8aac1fe67868d4c58c0543e08b305d838bb3d4a1
Max Lee <maxwell.a.lee56@gmail.com>
2019-05-01 17:33:03 -0400

    Copied over pets3 and edited the composer file

---
+++ model/validation-functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saman
+ * Date: 4/26/2019
+ * Time: 1:42 PM
+ */
+
+function validColor($color){
+    global $f3;
+    return in_array($color,$f3->get('colors'));
+}
+
+function validString($string){
+    if($string!="" && ctype_alpha($string)){
+        return true;
+    } else{
+        return false;
+    }
+}
